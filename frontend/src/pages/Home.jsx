59cb38f1fd077bd82627ab476e0c19537232e5e4
llasszzll@gmail.com <llasszzll@gmail.com>
2024-04-03 17:07:51 +0200

    Added frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa";
 
 function home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <section className="heading">
+        <h1>What do you need help with?</h1>
+        <p>Please choose from a option below</p>
+      </section>
+
+      <Link to="/new-ticket" className="btn btn-reverse btn-block">
+        <FaQuestionCircle /> Create New Ticket
+      </Link>
+      <Link to="/tickets" className="btn btn-block">
+        <FaTicketAlt /> View my tickets
+      </Link>
+    </>
   );
 }
 
